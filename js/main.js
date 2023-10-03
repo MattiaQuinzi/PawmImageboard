7e935fecadf2a7232290b0429749edb4b0fbbe0c
MattiaQuinzi <mattia.quinzi@studenti.unicam.it>
2023-10-03 15:24:16 +0200

    added js modules, created refresh function

--- js/main.js
+++ js/main.js
@@ -1,20 +1,106 @@
-import ImageBoard from "./imageboard";
-import Item from "*./item.js"
+import ImageBoard from "./imageboard.js";
+import Item from "./item.js";
 
-const ImageBoard = new ImageBoard();
+const imageboard = new ImageBoard();
 
-document.addEventListener("readystatechange"), (event) => {
-    if(event.target.readyState === "complete")
+document.addEventListener("readystatechange", (event) => {
+    if (event.target.readyState === "complete")
     {
         initApp();
     }
-};
+});
 
 const initApp = () => {
+    const itemEntryForm = document.getElementById("imgform");
+    itemEntryForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        processInput();
+    });
+
     refresh();
-}
+};
 
 const refresh = () => {
     clearBoardDisplay();
-    
+    renderBoard();
+};
+
+const clearBoardDisplay = () => {
+    const parentElement = document.getElementById("imageboard");
+    deleteContents(parentElement);
+    clearTextbox();
+};
+
+const deleteContents = (parentElement) => {
+    let child = parentElement.lastElementChild;
+    while (child)
+    {
+        parentElement.removeChild(child);
+        child = parentElement.lastElementChild;
+    }
+};
+
+const renderBoard = () => {
+    const board = imageboard.getBoard();
+    FileList.forEach(item => {
+        buildBoardItem(item);
+    });
+};
+
+const buildBoardItem = (item) => {
+    const div = document.createElement("div");
+    div.className = "item";
+
+    const img = document.createElement("img");
+    img.src = item.getUrl();
+    img.id = item.getId();
+
+    const label = document.createElement("label");
+    label.htmlFor = item.getId();
+    label.textContent = item.getTags();
+
+    div.appendChild(img);
+    div.appendChild(label);
+
+    const board = document.getElementById("imageboard");
+    board.appendChild(div);
+};
+
+const clearTextbox = () => {
+    document.getElementById("urlinput").value = "";
+    document.getElementById("tagsinput").value = "";
+};
+
+const processInput = () => {
+    const newUrl = getNewUrl();
+    const newTags = getNewTags();
+    if(!newUrl.length) return;
+    const nextItemId = getNextItemId();
+    const newItem = createNewItem(nextItemId, newUrl, newTags);
+};
+
+const getNewUrl = () => {
+    return document.getElementById("urlinput").value.trim();
+};
+
+const getNewTags = () => {
+    return document.getElementById("tagsinput").value.trim();
+};
+
+const getNextItemId = () => {
+    let nextItemId = 1;
+    const board = imageboard.getBoard();
+    if(board.length > 0)
+    {
+        nextItemId = board[board.length -1].getId() + 1;
+    }
+    return nextItemId;
+};
+
+const createNewItem = (id, url, tags) => {
+    const item = new Item();
+    item.setId(id);
+    item.setUrl(url);
+    item.setTags(tags);
+    return item;
 }
